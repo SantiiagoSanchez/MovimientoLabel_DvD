7686ae7214f21716a5ae4ecfac99924b5f2c2241
Sanch <Sanch@DESKTOP-NQV1S7L>
2023-10-13 13:08:32 -0300

    Agregar archivos de proyecto.

---
+++ MovimientoLabel/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace MovimientoLabel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btInicio = new System.Windows.Forms.Button();
+            this.lblPantalla = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btInicio
+            // 
+            this.btInicio.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btInicio.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btInicio.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btInicio.Location = new System.Drawing.Point(345, 385);
+            this.btInicio.Name = "btInicio";
+            this.btInicio.Size = new System.Drawing.Size(141, 53);
+            this.btInicio.TabIndex = 0;
+            this.btInicio.Text = "Iniciar/Start";
+            this.btInicio.UseVisualStyleBackColor = false;
+            this.btInicio.Click += new System.EventHandler(this.btInicio_Click);
+            // 
+            // lblPantalla
+            // 
+            this.lblPantalla.AutoSize = true;
+            this.lblPantalla.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblPantalla.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPantalla.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblPantalla.Location = new System.Drawing.Point(296, 362);
+            this.lblPantalla.Name = "lblPantalla";
+            this.lblPantalla.Size = new System.Drawing.Size(250, 20);
+            this.lblPantalla.TabIndex = 1;
+            this.lblPantalla.Text = "Pulse el boton/Push de button";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblPantalla);
+            this.Controls.Add(this.btInicio);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+            //
+            // Figura
+            //
+       
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btInicio;
+        private System.Windows.Forms.Label lblPantalla;
+    }
+}
+
---
+++ MovimientoLabel/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovimientoLabel
+{
+    public partial class Form1 : Form
+    {
+        int velocidad = 50;
+        int pasoH = 10;
+        int pasoV = 5;
+        int posicionH = 0;
+        int posicionV = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btInicio_Click(object sender, EventArgs e)
+        {
+            btInicio.Visible = false;
+            lblPantalla.Visible = false;
+            Label figura = new Label();
+            figura.BackColor = Color.Blue;
+            figura.Text = "DvD";
+            figura.Font = new System.Drawing.Font("Arial Black", 16F);
+            figura.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            figura.Top = posicionV;
+            figura.Left = posicionH;
+            figura.Width = 70;
+            figura.Height = 50;
+
+            Controls.Add(figura);
+
+            for (int i = 0; i < 10000; i++)
+            {
+                posicionH = posicionH + pasoH;
+                figura.Left = posicionH;
+                posicionV = posicionV + pasoV;
+                figura.Top = posicionV;
+
+
+
+                if (posicionH + figura.Width > this.Width)
+                {
+                    pasoH = -1 * pasoH;
+                }
+                else if (posicionH < 0)
+                {
+                    pasoH = -1 * pasoH;
+                }
+
+                if (posicionV + figura.Height > this.Height)
+                {
+                    pasoV = -1 * pasoV;
+                }
+                else if (posicionV < 0)
+                {
+                    pasoV = -1 * pasoV;
+                }
+
+                await Task.Delay(velocidad);
+
+            }
+        }
+    }
+    }
+
